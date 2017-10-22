8d52848b10f083158da2648f41672f04129c4f31
Erik Dubois <erik.dubois@gmail.com>
2017-10-22 10:38:26 +0200

    aur

--- 300-software-AUR-repo-v1.sh
+++ 300-software-AUR-repo-v1.sh
@@ -18,13 +18,10 @@ set -e
 #only if you run into issues with that
 #sudo mount -o remount,size=5G,noatime /tmp
 
-sh AUR/install-arc-gtk-theme-git-v*.sh
 sh AUR/install-brackets-v*.sh
-sh AUR/install-xcursor-breeze-v*.sh
 #sh AUR/install-canon-mg7750-v*.sh
 sh AUR/install-conky-aureola-v*.sh
 sh AUR/install-dropbox-v*.sh
-sh AUR/install-font-manager-v*.sh
 sh AUR/install-gnome-screenshot-v*.sh
 sh AUR/install-gradio-v*.sh
 sh AUR/install-insync-v*.sh
@@ -35,7 +32,7 @@ sh AUR/install-radiotray-v*.sh
 sh AUR/install-sardi-extra-icons-v*.sh
 sh AUR/install-skypeforlinux-bin-v*.sh
 sh AUR/install-spotify-v*.sh
-sh AUR/install-teamviewer-v*.sh
+#sh AUR/install-teamviewer-v*.sh
 sh AUR/install-ttf-mac-fonts-v*.sh
 sh AUR/install-virtualbox-for-linux-kernel-v*.sh
 #sh AUR/install-virtualbox-for-linux-lts-kernel-v*.sh
