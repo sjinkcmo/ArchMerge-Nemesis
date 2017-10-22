f6694ae5fddf41160c2acbeeac0adb780d8a71bf
Erik Dubois <erik.dubois@gmail.com>
2017-10-22 10:11:10 +0200

    update with aur folder

--- git-v1.sh
+++ git-v1.sh
@@ -1,49 +1,14 @@
 #!/bin/bash
-#
 ##################################################################################################################
-# Written to be used on 64 bits computers
 # Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-##################################################################################################################
+# Website 	: 	https://www.erikdubois.be
+# Website	:	https://www.archmerge.com
+# Website	:	https://www.archmerged.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
-
-# echo "# Ultimate-Linux-Mint-18" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git remote add origin https://github.com/erikdubois/Ultimate-Linux-Mint-18-Cinnamon.git
-# git push -u origin master
-
-# git config --global user.name x
-# git config --global user.email x
-# sudo git config --system core.editor nano
-# git config --global credential.helper cache
-# git config --global credential.helper 'cache --timeout=3600'
-
-
-# Force git to overwrite local files on pull - no merge
-
-# git fetch all
-
-# git push --set-upstream origin master
-
-# git reset --hard orgin/master
-
-
-# checking if kernel files are present otherswise github will become too big
-
-if [ -f linux* ]; then
-	echo "####################################"
-    	echo "Stopping the script!!"
-    	echo "Wait for the kernel update script to quit."
-    	echo "####################################"
-    	exit 0
-fi
-
 
 # checking if I have the latest files from github
 echo "Checking for newer files online first"
@@ -60,8 +25,8 @@ echo "####################################"
 read input
 
 # Committing to the local repository with a message containing the time details and commit text
-curtime=$(date)
-git commit -m "Comment : $input on $curtime"
+
+git commit -m "$input"
 
 # Push the local files to github
 
